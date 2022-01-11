14f054ef3dbdc7f9fb74ef72440dfbbdc279225a
John Schlesinger <johnschlesinger5@gmail.com>
2022-01-11 22:56:02 +0000

    Playlist page layout

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,9 +1,28 @@
 import { Box } from '@chakra-ui/layout';
+import GradientLayout from '../../components/GradientLayout';
 import { validateToken } from '../../lib/auth';
 import prisma from '../../lib/prisma';
 
+const getBackgroundColor = (id) => {
+    const colors = ['red', 'green', 'blue', 'orange', 'gray', 'purple'];
+
+    return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
 const Playlist = ({ playlist }) => {
-    return <Box>{playlist.name}</Box>;
+    const color = getBackgroundColor(playlist.id);
+    return (
+        <GradientLayout
+            color={color}
+            roundImage={false}
+            title={playlist.name}
+            subtitle="playlist"
+            description={`${playlist.songs.length} songs`}
+            image={`https://picsum.photos/400?random=${playlist.id}`}
+        >
+            <Box></Box>
+        </GradientLayout>
+    );
 };
 
 export const getServerSideProps = async ({ query, req }) => {
