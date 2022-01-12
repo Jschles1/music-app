613436f2bc4d62c0bdd54aa2e143dfa66dec83a9
John Schlesinger <johnschlesinger5@gmail.com>
2022-01-12 22:34:28 +0000

    Complete SongsTable format

--- components/SongsTable.tsx
+++ components/SongsTable.tsx
@@ -16,17 +16,40 @@ const SongTable = ({ songs }) => {
                         isRound
                     />
                 </Box>
-                
+
                 <Table variant="unstyled">
-                    <Thead borderBottom="1px solid" borderColor="rgba(255,255,255,0.2)">
+                    <Thead
+                        borderBottom="1px solid"
+                        borderColor="rgba(255,255,255,0.2)"
+                    >
                         <Tr>
                             <Th>#</Th>
                             <Th>Title</Th>
+                            <Th>Date Added</Th>
                             <Th>
                                 <AiOutlineClockCircle />
                             </Th>
                         </Tr>
                     </Thead>
+                    <Tbody>
+                        {songs.map((song, i) => (
+                            <Tr
+                                sx={{
+                                    transition: 'all .3s',
+                                    '&:hover': {
+                                        bg: 'rgba(255,255,255, 0.1)',
+                                    },
+                                }}
+                                key={song.id}
+                                cursor="pointer"
+                            >
+                                <Td>{i + 1}</Td>
+                                <Td>{song.name}</Td>
+                                <Td>{song.createdAt.toString()}</Td>
+                                <Td>{song.duration}</Td>
+                            </Tr>
+                        ))}
+                    </Tbody>
                 </Table>
             </Box>
         </Box>
